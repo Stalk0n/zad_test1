1a54997056b715b2750bad0094790715003bcd9f
Stalkon <139078777+Stalk0n@users.noreply.github.com>
2026-09-09 12:16:40 +0200

    fixes according to feedback

--- Assets/Scripts/SphereManager.cs
+++ Assets/Scripts/SphereManager.cs
@@ -1,109 +1,105 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Splines;
 using UnityEngine.UI;
 
 public class SphereManager : MonoBehaviour
 {
-    public float radius = 5f;
-    public float spiralSpeed = 0.5f;
-    public float maxSpeed = 1.0f;
-    public float speed = 0f;
-    private float accumulatedAngle = 0f;
-    private Vector3 center  = Vector3.zero;
-    private CharacterController controller;
-    private Renderer rend;
-    private bool hasStarted = false;
-    public Button startButton;
-    public TextMeshProUGUI distanceTraceledText;
-    public ParticleSystem fireworks;
-    public bool fireworksTriggered = false;
-    private float totalDistanceTraveled = 0f;
-    private Vector3 lastPosition;
+    [SerializeField] private SplineContainer spline;
+    [SerializeField] private Button startButton;
+    [SerializeField] private TextMeshProUGUI distanceTraveledText;
+    [SerializeField] private ParticleSystem fireworks;
+    [SerializeField] private Gradient colorGradient;
+
+    private float _maxSpeed = 8.0f;
+    private float _speed;
+    private bool _fireworksTriggered;
+    private float _accumulatedAngle;
+    private CharacterController _controller;
+    private Renderer _rend;
+    private bool _hasStarted;
+    private float _totalDistanceTraveled;
+    private float _distance;
+
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        lastPosition = transform.position;
-        speed = 0f;
+        _rend = GetComponent<Renderer>();
+        _speed = 0f;
+        _distance = 0f;
     }
+
     void Update()
     {
-        accumulatedAngle += speed * Time.deltaTime;
-        float currentRadius = radius * accumulatedAngle * spiralSpeed;
-        float x = center.x + Mathf.Cos(accumulatedAngle) * currentRadius;
-        float z = center.z + Mathf.Sin(accumulatedAngle) * currentRadius;
-        
-        transform.position = new Vector3(x, center.y, z);
-        
-        float distanceThisFrame = Vector3.Distance(transform.position, lastPosition);
-        totalDistanceTraveled += distanceThisFrame;
-        lastPosition = transform.position;
-        
-        float t = accumulatedAngle;
-        float r = Mathf.Sin(t) * 0.5f + 0.5f;
-        float g = Mathf.Sin(t + 2f) * 0.5f + 0.5f;
-        float b = Mathf.Sin(t + 4f) * 0.5f + 0.5f;
-        rend.material.color  = new Color(r, g, b);
+        float length = spline.CalculateLength();
+        _distance += _speed * Time.deltaTime;
+        _distance = Mathf.Min(_distance, length);
+        float t = _distance / length;
+        float progress = _distance / length;
 
-        if (currentRadius >= 8f && !fireworksTriggered)
+        transform.position = spline.EvaluatePosition(t);
+
+        _rend.material.color = colorGradient.Evaluate(progress);
+
+        if (_distance >= length)
         {
-            fireworksTriggered = true;
-            speed = 0f;
+            _fireworksTriggered = true;
+            _speed = 0f;
             StartCoroutine(FadeOutAndFire());
         }
     }
+
     public void OnStartButtonPressed()
     {
-        if (hasStarted) return;
-        hasStarted = true;
+        if (_hasStarted) return;
+        _hasStarted = true;
         startButton.gameObject.SetActive(false);
-        Debug.Log("Movement started!");
         StartCoroutine(RestoreSpeedGradually(0f, 2f));
     }
+
     public void OnSpacePressed()
     {
-        Debug.Log("Space pressed!");
-        distanceTraceledText.gameObject.SetActive(true);
-        distanceTraceledText.text = "Distance: " + totalDistanceTraveled.ToString("F2");
+        distanceTraveledText.gameObject.SetActive(true);
+        distanceTraveledText.text = "Distance: " + _distance.ToString("F2");
         StopAllCoroutines();
-        speed = 0;
+        _speed = 0;
         StartCoroutine(RestoreSpeedGradually(5f, 2f));
     }
 
     private IEnumerator RestoreSpeedGradually(float delay, float rampDuration)
     {
-        
         yield return new WaitForSeconds(delay);
 
         float elapsed = 0f;
-        float startSpeed = speed;
+        float startSpeed = _speed;
 
         while (elapsed < rampDuration)
         {
             elapsed += Time.deltaTime;
             float t = elapsed / rampDuration;
-            speed = Mathf.Lerp(startSpeed, maxSpeed, t);
+            _speed = Mathf.Lerp(startSpeed, _maxSpeed, t);
             yield return null;
         }
-        distanceTraceledText.gameObject.SetActive(false);
-        speed = maxSpeed; 
+
+        distanceTraveledText.gameObject.SetActive(false);
+        _speed = _maxSpeed;
     }
+
     private IEnumerator FadeOutAndFire()
     {
         float duration = 1f;
         float elapsed = 0f;
-        Color startColor = rend.material.color;
+        Color startColor = _rend.material.color;
 
         while (elapsed < duration)
         {
             elapsed += Time.deltaTime;
             float a = Mathf.Lerp(1f, 0f, elapsed / duration);
-            rend.material.color = new Color(startColor.r, startColor.g, startColor.b, a);
+            _rend.material.color = new Color(startColor.r, startColor.g, startColor.b, a);
             yield return null;
         }
-        rend.gameObject.SetActive(false);
+
+        _rend.gameObject.SetActive(false);
         fireworks.transform.position = transform.position;
         fireworks.gameObject.SetActive(true);
         fireworks.Play();
---
+++ Assets/Scripts/SpiralSpline.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+[ExecuteAlways]
+public class SpiralSpline : MonoBehaviour
+{
+    [SerializeField] int turns = 3;
+    [SerializeField] float startRadius = 1f;
+    [SerializeField] float endRadius = 10f;
+    [SerializeField] int pointsPerTurn = 20;
+
+    void OnValidate()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        var spline = GetComponent<SplineContainer>();
+        spline.Spline.Clear();
+
+        int count = turns * pointsPerTurn + 1;
+
+        for (int i = 0; i < count; i++)
+        {
+            float t = (float)i / (count - 1);
+            float angle = t * turns * 2f * Mathf.PI;
+            float radius = Mathf.Lerp(startRadius, endRadius, t);
+
+            spline.Spline.Add(new BezierKnot(new Vector3(
+                Mathf.Cos(angle) * radius,
+                0,
+                Mathf.Sin(angle) * radius
+            )));
+        }
+
+        spline.Spline.SetTangentMode(TangentMode.AutoSmooth);
+    }
+}
